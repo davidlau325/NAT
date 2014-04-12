32fcd5d8e27db685b493320efa0e79bc681ba7ed
David Lau <vx.drizzle@gmail.com>
2014-04-12 18:26:55 +0800

    Partially done TCP

--- tcp.h
+++ tcp.h
@@ -1,6 +1,21 @@
 #ifndef TCP_H
 #define TCP_H
 
-void handle_tcp(struct iphdr *ip, struct tcphdr *tcp)
+extern ipq_packet_msg_t *msg;
+extern unsigned int public_IP;
+extern unsigned int LOCAL_NETWORK;
+extern unsigned int LOCAL_MASK;
+extern char PORTARRAY[2001];
+
+struct TCPEntry{
+	unsigned int originalIP;
+	unsigned short originalPort;
+	unsigned short newPort;
+	int exitFlow;
+};
+
+typedef struct TCPEntry TCP_Table;
+
+int handle_tcp()
 
 #endif
